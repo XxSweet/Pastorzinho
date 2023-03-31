7178ca5833375b6e0c43545b8146f8402999c0df
ETEC Adolpho Berezin <e107ti@etec.sp.gov.br>
2023-03-31 19:54:05 -0300

    atualizei o exercicio

---
+++ Program.cs
@@ -0,0 +1,5 @@
+﻿Console.WriteLine("*** O Pastorzinho ***");
+Console.WriteLine("dó, ré, mi, fá, fá, fá,");
+Console.WriteLine("dó, ré, dó, ré, ré, ré,");
+Console.WriteLine("dó, sol, fá, mi, mi, mi,");
+Console.WriteLine("dó, ré, mi, fá, fá, fá.");
